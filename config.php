d4296fe55fb909c1df4a3f5ee205e79a9ded00c2
jack-ridder <935758882@qq.com>
2019-07-19 10:12:41 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,31 @@
+<?php
+
+$FromName = 'TUTAOCRM';
+
+$allow_sep = '60';  // 60秒内不能重复提交订单，防恶意刷单
+
+$sepmsg = '对不起！您已经提交过，不要重复提交，请等待60秒后再试！';  // 重复提交后弹出的提示语
+
+$MailFrom = 'smtp.91tutao.com'; 
+
+$Mailhost = 'test';
+
+$MailUsername = 'test';
+
+$MailPassword = 'test'; 
+
+$MailTo = 'test@91tutao.com'; 
+
+$MailTob = 'test@91tutao.com'; 
+
+$payzk = '1';
+
+$paytype = '01';
+
+$pay_config['partner']      = 'test'; 
+
+$pay_config['key']          = 'test';
+
+$pay_config['seller_email'] = 'test';
+
+?>
